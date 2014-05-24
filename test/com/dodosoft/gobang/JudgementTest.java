fa0e7f43bec57771eb78de284866282e25660f62
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2014-05-24 16:22:59 +0900

    Add test class for Judgement.java.

---
+++ test/com/dodosoft/gobang/JudgementTest.java
@@ -0,0 +1,115 @@
+package com.dodosoft.gobang;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class JudgementTest {
+
+    @Test
+    public void testOnMark_right() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(i, 0, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_left() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(5 - i - 1, 0, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_down() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(0, i, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_up() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(0, 5 - i - 1, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_leftUp() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(5 - i - 1, 5 - i - 1, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_rightUp() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(i, 5 - i - 1, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_leftDown() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(5 - i - 1, i, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+    @Test
+    public void testOnMark_rightDown() throws Exception {
+        final Judgement sut = new Judgement();
+        final GobangModel model = new ArrayGobangModel(19, 19);
+        model.addListener(sut);
+
+        assertThat(sut.getWinner(), is((Go)null));
+        for (int i = 0; i < 5; i++) {
+            model.mark(i, i, Go.WHITE);
+        }
+        assertThat(sut.getWinner(), is(Go.WHITE));
+    }
+
+}
