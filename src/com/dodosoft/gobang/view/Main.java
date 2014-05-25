12558aef27a7dfbdd7eb9895e76fdf6935765cd4
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2014-05-25 12:10:18 +0900

    アプリケーションアイコンとタイトルを設定しました。

--- src/com/dodosoft/gobang/view/Main.java
+++ src/com/dodosoft/gobang/view/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -18,8 +19,8 @@ public class Main extends Application {
 
         Controller ctrl = loader.getController();
         ctrl.start();
-
-        primaryStage.setTitle("Hello World");
+        primaryStage.getIcons().add(new Image("/com/dodosoft/gobang/view/sample-icon.png"));
+        primaryStage.setTitle("Gobang");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.sizeToScene();
